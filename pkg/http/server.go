6fe939770bfb62bc7e456dbe1fe9f0f00bd667b7
Marcelo Aguero <marceloaguero@gmail.com>
2019-10-28 21:44:25 -0300

    Refactor HTTP server

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -2,11 +2,36 @@ package http
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/marceloaguero/vault/pkg/endpoint"
 )
+
+func decodeHashRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req endpoint.HashRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func decodeValidateRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req endpoint.ValidateRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
 
 // NewHTTPServer makes a new Vault HTTP service
 func NewHTTPServer(ctx context.Context, endpoints endpoint.Endpoints) http.Handler {
